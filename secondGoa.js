10d8b7e302702154de4893316a02b8ac5423df70
Gokul Chandra Kandpal <kandpalgokul01@gmail.com>
2022-07-23 01:29:02 +0530

    Day 4 Both Page

---
+++ secondGoa.js
@@ -0,0 +1,279 @@
+let websiteData=[
+    {
+    Ammenities: ['Swimming Pool', 'Air Conditioner', 'Internet', 'Television', 'Parking', 'Housekeeping', 'Washing Machine', 'Refrigerator', 'Spa', 'Cook On'],
+    bathroom: "4",
+    bedroom: "5",
+    city: "Goa",
+    costpernight: 10000,
+    country: "India",
+    img_url: "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/55791061/ed5eb2012c2711e8b2ea0a92a929817e_watermarked_image_1024.jpeg",
+    maxguests: "10",
+    refid: "200",
+    title: "Jomtien Paradise Villa | 5 Bed Property With Jet Pool And Sauna In Goa",
+    type_of_property: "Villa",
+    Rating:4,
+    Rated:"Excelent Stay",
+    },
+    {
+    Ammenities:['Swimming Pool', 'Air Conditioner', 'Internet', 'Television'],
+    bathroom: "5",
+    bedroom: "5",
+    city: "Goa",
+    costpernight: 80000,
+    country: "India",
+    img_url: "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/46007896/0b5364faa2c711e783140a92a929817e_watermarked_image_1024.jpeg",
+    
+    maxguests: "10",
+    refid: "201",
+    title: "Baan Chokdee | 5 Bed Pool Villa Near Jomtien Beach In South Goa",
+    type_of_property: "Villa",
+    Rating:5,
+    Rated:"Pleasent Stay",
+    },
+    {
+    Ammenities:['Swimming Pool', 'Air Conditioner', 'Internet', 'Television', 'Parking', 'Spa'],
+    bathroom: "2",
+    bedroom: "2",
+    city: "Goa",
+    costpernight: 4000,
+    country: "India",
+    img_url: "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/36952294/7819757aa2c511e783140a92a929817e_watermarked_image_1024.jpeg",
+    maxguests: "5",
+    refid: "202",
+    title: "Pratumnak Regal Villa | 2 Bed Pool Home At Pratumnak Hill In Goa",
+    type_of_property: "Hotel/Delux",
+    Rating:"3",
+    Rated:"Good for Stay",
+    },
+    {
+    Ammenities:['Internet', 'Television', 'Parking'],
+    bathroom: "1",
+    bedroom: "2",
+    city: "Goa",
+    costpernight: 3000,
+    country: "India",
+    img_url: "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/72289717/255_watermarked_image_1024.jpeg",
+    maxguests: "4",
+    refid: "203",
+    title: "Boutique Resort For Rent - Pepper Villa (2 Bedrooms) Stay",
+    type_of_property: "Farm",
+    Rating:"5",
+    Rated:"Excelent Stay",
+    },
+    {
+    Ammenities:['Swimming Pool', 'Air Conditioner', 'Internet', 'Parking', 'Spa', 'Cook On'],
+    bathroom: "2",
+    bedroom: "3",
+    city: "Goa",
+    costpernight: 5000,
+    country: "India",
+    img_url: "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/32358195/82ededb7a2ca11e783180a92a929817e_watermarked_image_1024.jpeg",
+    maxguests: "6",
+    refid: "204",
+    title: "Thammachat P3 Victoria | 3 Bedroom Pattaya Pool Villa In Bangsaray",
+    type_of_property: "Home Stay",
+    Rating:"4.5",
+    Rated:"Pleasent Stay",
+    },
+
+
+    
+      {
+        Ammenities: [
+          "Swimming Pool",
+          "Air Conditioner",
+          "Housekeeping",
+          "Functional Kitchen",
+          "Washing Machine",
+          "Dish Washer",
+        ],
+        bathroom: "4",
+        bedroom: "4",
+        city: "goa",
+        costpernight: 20000,
+        country: "India",
+        img_url:
+          "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/29525985/85be9537001f11e890160a8e1b1ce4da_watermarked_image_1024.jpeg",
+        maxguests: "12",
+        refid: "",
+        title: "Luxury Villa With Private Swimming Pool",
+        type_of_property: "Villa",
+        Rating:"5",
+        Rated:  "Peace belongs here",
+      },
+    
+      {
+        Ammenities: ["Internet", "Parking", "Refrigerator"],
+        bathroom: "1",
+        bedroom: "1",
+        city: "goa",
+        costpernight: 4000,
+        country: "India",
+        img_url:
+          "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/87277489/bn4a2377_watermarked_image_1024.jpeg",
+        maxguests: "4",
+        refid: "456",
+        title: "",
+        type_of_property: "Homestay",
+        Rating:"4.5",
+        Rated:"Relaxing",
+      },{
+        Ammenities: [
+          "Air Conditioner",
+          "Internet",
+          "Television",
+          "Parking",
+          "Functional Kitchen",
+          "Refrigerator",
+          "Spa",
+          "Cook On",
+        ],
+        bathroom: "2",
+        bedroom: "2",
+        city: "goa",
+        costpernight: 8000,
+        country: "India",
+        img_url:
+          "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/20236617/d8d719d1038011ea92530a8e1b1ce4da_watermarked_image_1024.jpeg",
+        maxguests: "4",
+        refid: "791",
+        title: "2 Bedroom Ac Villa",
+        type_of_property: "Villa",
+        Rating:"4",
+    Rated:"Amazing Expeience here",
+      },
+      {
+        Ammenities: [
+          "Air Conditioner",
+          "Internet",
+          "Parking",
+          "Housekeeping",
+          "Functional Kitchen",
+          "Dish Washer",
+          "Refrigerator",
+          "Gym",
+          "Spa",
+        ],
+        bathroom: "3",
+        bedroom: "3",
+        city: "goa",
+        costpernight: 7000,
+        country: "India",
+        img_url:
+          "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/82258333/p6050305_watermarked_image_1024.jpeg",
+        maxguests: "8",
+        refid: "654",
+        title: "3 Bedroom Bungalow At Villa Venus In Candolim, Goa",
+        type_of_property: "Villa",
+        Rating:"3",  
+        Rated:"All good",
+      },
+    
+      {
+        Ammenities: [
+          "Swimming Pool",
+          "Air Conditioner",
+          "Internet",
+          "Television",
+          "Functional Kitchen",
+        ],
+        bathroom: "2",
+        bedroom: "2",
+        city: "goa",
+        costpernight: "5000",
+        country: "India",
+        img_url:
+          "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/30910222/screenshot008_watermarked_image_1024.jpeg",
+        maxguests: "6",
+        refid: "897",
+        title: "2bhk Pool Facing Apartment.",
+        type_of_property: "Villa",
+        Rating:"3",
+       Rated:"Affordable",
+      },
+    
+    ]
+
+    let container=document.querySelector("#dataContainer")
+function sort(){
+
+
+    let val=document.querySelector("#SortByPrice");
+    
+    if(val.value=="Relevance"){
+      
+       
+        displayData(websiteData)
+        console.log(val.value)
+    }else if(val.value=="Low To High"){
+        websiteData.sort(function(a,b){
+            return a.costpernight-b.costpernight;
+        })
+        console.log(val.value)
+        container.innerHTML=null;
+        displayData(websiteData)
+    }else{
+        websiteData.sort(function(a,b){
+            return b.costpernight-a.costpernight;
+        })
+        console.log(val.value)
+        container.innerHTML=null;
+        displayData(websiteData)
+    }
+}
+    
+    displayData(websiteData)
+    function displayData(data){
+        
+       
+        data.forEach(function(elem){
+            let div=document.createElement("div")     
+                     
+            let div1=document.createElement("div")                
+            let a=document.createElement("a")
+            let div2=document.createElement("div")
+            div.setAttribute("id","cont")
+            div2.setAttribute("id","cont2")
+
+            a.onclick=function(){
+               saveInLocal(elem)
+               a.href="#"
+            }
+
+            let image=document.createElement("img")
+            image.src=elem.img_url;
+            let ref=document.createElement("p")
+            ref.innerText=`Ref Id #${elem.refid}`
+            ref.style.color="grey"
+            let type=document.createElement("p")
+            type.innerText=`${elem.type_of_property} Stay`
+            type.style.color="blue"
+            let city=document.createElement("p")
+            city.innerText=`${elem.city}, ${elem.country}`
+            let title=document.createElement("p")
+            title.innerText=elem.title
+            let additional=document.createElement("p")
+            
+            additional.innerText=`${elem.type_of_property}| AC| ${elem.bathroom}Bath| ${elem.bedroom}Bed| `
+            additional.style.color="blue"
+            let price=document.createElement("p")
+            price.innerText=`₹${elem.costpernight} per night`
+          div2.append(ref,title,type,city,additional,price)
+            div1.append(image)
+            div.append(div1,div2)
+            a.append(div)
+            container.append(a)
+        })
+    }
+    
+
+    function saveInLocal(elem){
+
+        let bookdata=JSON.parse(localStorage.getItem("book"))||[]
+        bookdata.push(elem)
+        localStorage.setItem("book",JSON.stringify(bookdata))
+        console.log(bookdata)
+    }
+
+
+  
