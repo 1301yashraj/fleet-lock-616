3b58450851acf1529ffe45ba76d0dd1a85168ce2
tarun-upadhyay <tarunu88@gmail.com>
2022-07-23 00:18:06 +0530

    booking

---
+++ booking1178.js
@@ -0,0 +1,64 @@
+
+let country = document.getElementById("ins_country")
+country.innerText = "country_changing";
+
+let city = document.getElementById("ins_city");
+city.innerText = "city_changing";
+
+let property_id = document.getElementById("ins_property")
+property_id.innerText = "12_chagning";
+
+let title = document.getElementById("title_h3");
+title.innerText = "title_changing";
+
+let address = document.getElementById("title_location");
+address.innerText = "title_changing";
+
+
+let avatar = document.getElementById("img_1");
+avatar.src =
+  "https://d2vcelvjdj7n25.cloudfront.net/media/property_photos/image_watermarked_1024/32762937/6c6b07cbc0c011e7ae460a8e1b1ce4da_watermarked_image_1024.jpeg";
+
+let property_type = document.getElementById("priV_button");
+property_type.innerText = "villa_changing"
+document.getElementById("typepropery").innerText = property_type.innerText;
+
+let bathroom = document.getElementById("bathroom_dom");
+bathroom.innerText = "3_CHANGING"
+
+let max_guest = document.getElementById("max_guest_dom");
+max_guest.innerText = "2_changing"
+document.getElementById("max_guest_button").innerText = max_guest.innerText;
+
+let cost_p_night = document.getElementById("cost_per_night");
+cost_p_night.innerText = "727_chang"
+document.getElementById("cost_pe_total").innerText = cost_p_night.innerText;
+
+let ammenit_1 = document.getElementById("amm1");
+ammenit_1.innerText = "wif_chang"
+
+let ammenit_2 = document.getElementById("amm2");
+ammenit_2.innerText = "wac_chang";
+
+
+let ammenit_3 = document.getElementById("amm3");
+ammenit_3.innerText = "ac_chang"
+
+let ammenit_4 = document.getElementById("amm4");
+ammenit_4.innerText = "sminming_chang"
+
+
+
+
+
+// let mapframe = document.getElementById("gmap_canvas")
+// mapframe.src = `https://maps.google.com/maps?q=${china}&t=&z=13&ie=UTF8&iwloc=&output=embed`
+// `
+
+
+// function myfunc(){
+//    let no_o_guest =  +document.getElementById("guests").value 
+//    let start_date = document.getElementById("from_date").value;
+//    let end_date = document.getElementById("from_date").value;
+//    console.log(start_date)
+// }
